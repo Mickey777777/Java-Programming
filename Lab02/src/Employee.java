06ffcdfc1baa89f3f825d83d077ce8ad53e4b1fc
Mickey777777 <wh879642@gmail.com>
2025-12-30 09:48:47 +0900

    Lab02

---
+++ Lab02/src/Employee.java
@@ -0,0 +1,61 @@
+// 2025005847 박우현 
+public class Employee {
+	private int age;
+	private String fullName;
+	private String position;
+	private int salary;
+	
+	public Employee() {
+		this.age = 0;
+		this.fullName = "";
+		this.position = "";
+		this.salary = 0;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setName(String firstname, String lastname) {
+		this.fullName = firstname + " " + lastname;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getName() {
+		return this.fullName;
+	}
+	
+	public int getSalary() {
+		return this.salary;
+	}
+	
+	public String getPosition() {
+		return this.position;
+	}
+	
+	public void printPosition() {
+		System.out.printf("%-20s: %-19s\n", this.fullName, this.position);
+	}
+	
+	public void printSalary() {
+		System.out.printf("%-20s: %,d\n", this.fullName, this.salary);
+	}
+	
+	public void printEmployee() {
+		System.out.printf("%-23s%2d%23s    %,d\n", this.fullName, this.age, this.position, this.salary);
+	}
+	
+	
+	
+}
---
+++ Lab02/src/EmployeeApp.java
@@ -0,0 +1,99 @@
+// 2025005847 박우현 
+import java.util.Scanner;
+
+public class EmployeeApp {
+	
+	private static final int MAX_NUM = 5;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Employee[] employee = new Employee[MAX_NUM];
+		
+		for(int i=0; i<MAX_NUM; i++) employee[i] = new Employee(); 
+		
+		employee[0].setAge(34);
+		employee[0].setName("Alan", "Cooper");
+		employee[0].setPosition("Senior Engineer");
+		employee[0].setSalary(6500000);
+		
+		employee[1].setAge(26);
+		employee[1].setName("Brendan", "Eich");
+		employee[1].setPosition("Junior Engineer");
+		employee[1].setSalary(5000000);
+		
+		employee[2].setAge(38);
+		employee[2].setName("Dennis", "Richie");
+		employee[2].setPosition("Chief Engineer");
+		employee[2].setSalary(7800000);
+		
+		employee[3].setAge(42);
+		employee[3].setName("Larry", "Wall");
+		employee[3].setPosition("Team Leader");
+		employee[3].setSalary(8200000);
+		
+		employee[4].setAge(46);
+		employee[4].setName("Richard", "Stallman");
+		employee[4].setPosition("Project Manager");
+		employee[4].setSalary(9000000);
+		
+		int menu;
+		
+		while(true) {
+			printMenu();
+			menu = sc.nextInt();
+			
+			if(menu == 1) {
+				displayAllPositions(employee);
+			}else if(menu == 2) {
+				displayAllSalary(employee);
+			}else if(menu == 3) {
+				displayAllInformation(employee);
+			}else if(menu == 0) {
+				System.out.println("Bye");
+				break;
+			}else {
+				System.out.println("Wrong number. Type again!");
+			}
+		}
+	
+		sc.close();
+	}
+	
+	private static void printMenu() {
+		System.out.println("========================================");
+		System.out.println("1. Display all positions");
+		System.out.println("2. Display all salaries");
+		System.out.println("3. Display all information");
+		System.out.println("0. Quit");
+		System.out.println("========================================");
+		System.out.print("-> ");
+	}
+	
+	private static void displayAllPositions(Employee[] employee) {
+		System.out.println("----------------------------------------");
+		System.out.printf("%-20s %-20s\n", "Name", "Position");
+		System.out.println("----------------------------------------");
+		for(int i=0; i<MAX_NUM; i++) {
+			employee[i].printPosition();
+		}
+	}
+	
+	private static void displayAllSalary(Employee[] employee) {
+		System.out.println("----------------------------------------");
+		System.out.printf("%-20s %-20s\n", "Name", "Salary");
+		System.out.println("----------------------------------------");
+		for(int i=0; i<MAX_NUM; i++) {
+			employee[i].printSalary();
+		}
+	}
+	
+	private static void displayAllInformation(Employee[] employee) {
+		System.out.println("---------------------------------------------------------------");
+		System.out.printf("%-22s%-18s%-15s%-8s\n", "Name", "Age", "Position", "Salary");
+		System.out.println("---------------------------------------------------------------");
+		for(int i=0; i<MAX_NUM; i++) {
+			employee[i].printEmployee();
+		}
+	}
+
+}
